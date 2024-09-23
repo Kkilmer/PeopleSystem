9807cc9a2d93d6f0579a241df5a84eb2e5e1ec3b
KevinK <kp827767@gmail.com>
2024-09-23 20:37:37 -0300

    ProjetoNew

---
+++ src/tech/kevin/main/App.java
@@ -0,0 +1,16 @@
+package tech.kevin.main;
+
+import tech.kevin.pessoas.Pessoa;
+
+public class App {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+        String nome = "Kevin";
+
+
+        // CADASTRAR CLIENTES
+
+        // CADASTRAR FUNCIONARIOS
+    }
+
+}
---
+++ src/tech/kevin/pessoas/Pessoa.java
@@ -0,0 +1,19 @@
+package tech.kevin.pessoas;
+
+import tech.kevin.composicao.Endereco;
+import tech.kevin.composicao.Telefone;
+
+public class Pessoa {
+    private String nome;
+    private String dataNascimento;
+    private Endereco endereco;
+    private Telefone telsContato;
+
+    public void cadastrar(){
+
+    }
+
+    public String obterIdade(){
+        return "";
+    }
+}
